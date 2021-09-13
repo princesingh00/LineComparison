1de93c2a9cdcffce93f38644f86b3e82a8b5faa3
PrinceSingh00 <singhprince0088@gmail.com>
2021-09-13 10:07:57 +0530

    ADD line class and length feature

--- src/main/java/com/bl/linecomparison/LineComparison.java
+++ src/main/java/com/bl/linecomparison/LineComparison.java
@@ -4,5 +4,8 @@ public class LineComparison {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+
+        Line line = new Line(8,3,3,-2);
+        line.length();
     }
 }
