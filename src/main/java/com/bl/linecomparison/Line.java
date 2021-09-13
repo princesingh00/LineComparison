dcdb73150d73e8ed2a6a7d28656cc5c5ae19aca5
PrinceSingh00 <singhprince0088@gmail.com>
2021-09-13 10:32:01 +0530

    ADD comparing line with equals method

--- src/main/java/com/bl/linecomparison/Line.java
+++ src/main/java/com/bl/linecomparison/Line.java
@@ -1,5 +1,7 @@
 package com.bl.linecomparison;
 
+import java.util.Objects;
+
 public class Line {
 
     public int x1, y1, x2, y2;
@@ -14,5 +16,21 @@ public class Line {
     public double length() {
         return Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) +
                 (this.y2 - this.y1) * (this.y2 - this.y1));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Line)) return false;
+        Line line = (Line) o;
+        return x1 == line.x1 &&
+                y1 == line.y1 &&
+                x2 == line.x2 &&
+                y2 == line.y2;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x1, y1, x2, y2);
     }
 }
--- src/main/java/com/bl/linecomparison/LineComparison.java
+++ src/main/java/com/bl/linecomparison/LineComparison.java
@@ -5,7 +5,10 @@ public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
 
-        Line line = new Line(8,3,3,-2);
-        line.length();
+        Line AB = new Line(8,3,3,-2);
+        Line CD = new Line(3,-2,8,3);
+
+        boolean equals = AB.equals(CD);
+        boolean length = AB.length() == CD.length();
     }
 }
